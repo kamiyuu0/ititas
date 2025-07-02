bfb166e6b1a9801309bc695e8a50db1d8531b5d2
yuekami <yuekami7@gmail.com>
2025-07-02 14:39:55 +0900

    feat: tasksテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :tasks, dependent: :destroy
   validates :name, presence: true, length: { maximum: 10 }, uniqueness: true
+
+  def own?(object)
+    id == object&.user_id
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_02_044228) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_02_052859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
+  create_table "tasks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description", null: false
+    t.date "target_date", null: false
+    t.boolean "done", default: false, null: false
+    t.boolean "is_public", default: true, null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "target_date"], name: "index_tasks_on_user_id_and_target_date", unique: true
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +39,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_02_044228) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "tasks", "users"
 end
