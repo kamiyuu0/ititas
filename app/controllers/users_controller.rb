02a530a467b10100929d2ac861d79d8518b2189b
yuekami <yuekami7@gmail.com>
2025-07-02 22:41:49 +0900

    feat: マイページで今日のタスクについての情報を表示するよう対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @tasks = @user.tasks.order(target_date: :desc)
+    @today_task = current_user.tasks.find_by(target_date: Date.today)
   end
 end
