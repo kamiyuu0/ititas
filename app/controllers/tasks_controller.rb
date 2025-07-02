572848e81ba2449e293cdd67be2457adbc97b657
yuekami <yuekami7@gmail.com>
2025-07-02 15:44:16 +0900

    feat: task関連のデザイン修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all.includes(:user).order(target_date: :asc)
+    @tasks = Task.all.includes(:user).where(is_public: true, target_date: Date.today).order(target_date: :asc)
   end
 
   def new
@@ -13,9 +13,12 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.build(task_params)
+    @task.target_date = Date.today
+
     if @task.save
       redirect_to task_path(@task), notice: "Task was successfully created."
     else
+      # TODO: エラーメッセージを表示するための処理を追加
       render :new
     end
   end
