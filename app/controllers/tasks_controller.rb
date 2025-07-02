68fb88250fc36779246292d11fd4fa45bdbb2d96
yuekami <yuekami7@gmail.com>
2025-07-02 15:18:48 +0900

    feat: tasksコントローラの作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,32 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all.includes(:user).order(target_date: :asc)
+  end
+
+  def new
+    if current_user.tasks.where(target_date: Date.today).exists?
+      redirect_to tasks_path, alert: "今日のタスクはすでに登録されています。"
+      return
+    end
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to task_path(@task), notice: "Task was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+  
+  def task_params
+    params.require(:task).permit(:title, :description, :target_date, :done, :is_public)
+  end
+end
