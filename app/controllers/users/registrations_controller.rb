72238f4f42451e5a217bcb5e5377fb1a4b8317af
yuekami <yuekami7@gmail.com>
2025-07-02 14:42:36 +0900

    feat: ログイン・サインアップ・ログアウトのリダイレクト先を修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -51,13 +51,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # The path used after sign up.
   def after_sign_up_path_for(resource)
     # サインアップ後にリダイレクトさせたいパスを記載
-    # TODO: ここは何かしらのパスを指定する必要があるかもしれない
-    root_path
+    tasks_path
   end
 
   # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
      # アクティブでないアカウントのサインアップ後にリダイレクトさせたいパスを記載(あんまわかってない)
-     root_path
+     tasks_path
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -27,7 +27,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def after_sign_in_path_for(resource)
     # TODO: サインイン後にリダイレクトさせたいパスを記載
-    root_path
+    tasks_path
   end
 
   def after_sign_out_path_for(resource)
