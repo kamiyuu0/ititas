eff942edd30baee4ea6371dfd6be5125747c4d9a
yuekami <yuekami7@gmail.com>
2025-07-03 16:53:04 +0900

    feat: Xシェアの検証

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   def index
     @tasks = Task.all.includes(:user).where(is_public: true, target_date: Date.today).order(target_date: :asc)
