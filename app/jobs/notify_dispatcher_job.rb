cbb762c5ad0de3b20402d2f8a04cea1aff586a2c
yuekami <yuekami7@gmail.com>
2025-09-26 09:43:35 +0900

    fix: 時間取得のタイムゾーンを削除し、リファクタリングTODOコメントのフォーマットを修正

--- app/jobs/notify_dispatcher_job.rb
+++ app/jobs/notify_dispatcher_job.rb
@@ -2,10 +2,10 @@ class NotifyDispatcherJob < ApplicationJob
   queue_as :default
 
   def perform
-    now = Time.current.in_time_zone("Asia/Tokyo").change(sec: 0)
-    target_time = Time.parse("2000-01-01 #{now.strftime('%H:%M')}:00") #pgのtime型のデフォルト年月日は2000-01-01
+    now = Time.current.change(sec: 0)
+    target_time = Time.parse("2000-01-01 #{now.strftime('%H:%M')}:00") # pgのtime型のデフォルト年月日は2000-01-01
 
-    #TODO:リファクタリング
+    # TODO:リファクタリング
     tmp_user = User.where(provider: "line", notification_enabled: true)
     tmp2_user = tmp_user.where("notification_time = ?", target_time)
     puts "================="
@@ -20,7 +20,7 @@ class NotifyDispatcherJob < ApplicationJob
       puts "================="
     end
     # logger
-    #Rails.logger "#target_time：#{target_time}"
+    # Rails.logger "#target_time：#{target_time}"
 
     tmp2_user.find_each do |user|
       puts user.name
